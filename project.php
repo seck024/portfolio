be46b6f1493a6b59d19b149e02570048d8437206
PapaSECK <seckpapaamadou96@gmail.com>
2025-04-25 10:33:27 +0200

    update

--- project.php
+++ project.php
@@ -1,11 +1,9 @@
 <?php
-// Importation des classes PHPMailer
 use PHPMailer\PHPMailer\PHPMailer;
 use PHPMailer\PHPMailer\SMTP;
 use PHPMailer\PHPMailer\Exception;
-// Vérifiez si le formulaire a été soumis via POST
+
 if ($_SERVER['REQUEST_METHOD'] == 'POST') {
-    // Assurez-vous que les champs nécessaires sont définis
     if (isset($_POST['name'], $_POST['email'], $_POST['message'])) {
         $nom = $_POST['name'];
         $email = $_POST['email'];
@@ -16,47 +14,48 @@ if ($_SERVER['REQUEST_METHOD'] == 'POST') {
         require 'PHPMailer/src/PHPMailer.php';
         require 'PHPMailer/src/SMTP.php';
 
-        // Créer une instance
+        // Chargement du fichier .env
+        if (file_exists(__DIR__ . '/.env')) {
+            $lines = file(__DIR__ . '/.env');
+            foreach ($lines as $line) {
+                if (strpos(trim($line), '=') !== false) {
+                    list($key, $value) = explode('=', trim($line), 2);
+                    $_ENV[$key] = $value;
+                }
+            }
+        }
+
         $mail = new PHPMailer(true);
 
         try {
-            // Configurations du serveur
             $mail->isSMTP();
             $mail->Host       = 'smtp.gmail.com';
             $mail->SMTPAuth   = true;
-            $mail->Username   = 'seck777696931@gmail.com';
-            $mail->Password   = 'oxxr loew arkf xyqs';
-            $mail->SMTPSecure = ''; // Désactiver SSL/TLS
+            $mail->Username   = $_ENV['MAIL_USERNAME'];
+            $mail->Password   = $_ENV['MAIL_PASSWORD'];
+            $mail->SMTPSecure = PHPMailer::ENCRYPTION_STARTTLS;
             $mail->Port       = 587;
 
-            // Destinataire
             $mail->setFrom('from@example.com', 'portfolio');
             $mail->addAddress('seckpapaamadou96@gmail.com');
 
-            // Contenu de l'e-mail
             $mail->isHTML(true);
             $mail->Subject = 'Message de Contact';
             $mail->Body    = $messageContent;
             $mail->AltBody = 'Message en texte brut';
 
-            // Envoyer l'e-mail
             $mail->send();
-
-            // Redirection avec un message de succès
             header('Location: index.html?status=success');
             exit;
         } catch (Exception $e) {
-            // Redirection avec un message d'erreur
             header('Location: index.html?status=error');
             exit;
         }
     } else {
-        // Si le formulaire n'a pas tous les champs nécessaires
         header('Location: index.html?status=error');
         exit;
     }
 } else {
-    // Si la requête n'est pas POST, on redirige vers la page d'accueil sans envoyer de mail
     header('Location: index.html');
     exit;
 }
